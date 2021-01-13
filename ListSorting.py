3db9add18eced9cf44a0a2c4d5409c849aa6981a
Prajna0408 <59387292+Prajna0408@users.noreply.github.com>
2021-01-13 14:38:00 +0530

    Add files via upload

---
+++ ListSorting.py
@@ -0,0 +1,10 @@
+#Sorting Numbers
+Numbers = [45, 21, 98, 100, -9, -108]
+Numbers.sort()
+print(Numbers)
+
+
+#Sorting Words
+Words = ["sorting", "Alphabets", "LHD", "MLH"]
+Words.sort()
+print(Words)
